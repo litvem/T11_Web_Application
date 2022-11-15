2f759e1744e848e43b2ce591acdc9bef582e6dcd
baoq <baoq@student.chalmers.se>
2022-11-15 23:44:34 +0100

    #1 Make sessions rolling
    Session expiry refreshes when the client is active

--- backend/src/util/session.js
+++ backend/src/util/session.js
@@ -5,6 +5,7 @@ require("dotenv").config()
 module.exports = session({
     secret: process.env.SESSION_SECRET,
     cookie: { maxAge: 3_600_000 }, // 1 hour
+    rolling: true, // Refresh expiration on reload
     resave: false,
     saveUninitialized: false // Set to true upon user login
 })

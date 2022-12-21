471b2a27bd3bb9a0a78fab5f185eabc7fae48074
baoq <baoq@student.chalmers.se>
2022-12-21 16:04:13 +0100

    #15 Style migration
    All styles have been moved to their respective Vue components.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,20 +3,18 @@ import App from "./App.vue";
 import router from "./router";
 import { BootstrapVue, BootstrapVueIcons } from "bootstrap-vue";
 // import * as VueGoogleMaps from 'vue2-google-maps'
-import VueGeoLocation from 'vue-browser-geolocation'
-import GmapVue from 'gmap-vue'
+import VueGeoLocation from "vue-browser-geolocation";
+import GmapVue from "gmap-vue";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
-import "./assets/main.css";
-
 Vue.use(GmapVue, {
   load: {
-    key: 'AIzaSyCP-QTMs7ocdRbmaL92dkzLXJLd0_LK2ag',
-    libraries: 'places'
-  }
-})
+    key: "AIzaSyCP-QTMs7ocdRbmaL92dkzLXJLd0_LK2ag",
+    libraries: "places",
+  },
+});
 Vue.use(VueGeoLocation);
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
